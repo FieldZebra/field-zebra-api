a7e8fa5f8595aa679a70ff8a133967ebc92ef2e8
Sami Hidmi <hidmi.6@osu.edu>
2021-04-14 14:48:02 -0400

    part 2

---
+++ backend/Field-Zebra-Api.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Field.Zebra.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Field_Zebra_Api.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {}
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
