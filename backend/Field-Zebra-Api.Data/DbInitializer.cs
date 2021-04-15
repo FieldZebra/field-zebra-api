514e5630aa12450e4a143bda6fa6b6b77f44d34b
Sami Hidmi <hidmi.6@osu.edu>
2021-04-15 17:06:35 -0400

    part changes

---
+++ backend/Field-Zebra-Api.Data/DbInitializer.cs
@@ -0,0 +1,23 @@
+using Field.Zebra.Domain.Catalog;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace Field.Zebra.Data
+{
+    public static class DbInitializer
+    { 
+        public static void Initializer(Field_Zebra_Api.Data.StoreContext context, ILogger logger)
+        {
+            if(!context.Items.Any())
+            {
+                var items = new Item[]
+                {
+                    new Item("T-Shirt", "Ohio State Block O", "Nike", 39.99m),
+                    new Item("Shorts", "Casual shorts", "Nike", 49.99m)
+                };
+                context.Items.AddRange(items);
+                context.SaveChanges();
+            }
+        }
+    }
+}
